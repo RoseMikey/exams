b4dd5075d5cf61423eb09f434df253c6dad99cab
Malika <malikaaliqulova665@gmail.com>
2024-05-03 18:30:39 +0500

    good

---
+++ main.py
@@ -0,0 +1,47 @@
+#1-masala--------------
+
+# def arrey():
+#     lst = ["1", "3", "3.6"]
+#     for x in range(0,2):
+#         lst = int
+#
+#     return lst
+# print(arrey())
+
+
+
+
+
+
+
+
+#2-masala-------------
+# def rate():
+#     lst = [1, 2, 3, 4, 5]
+#     lst = lst.pop(4)
+#     return lst
+# print(rate())
+# def rate():
+#     lst = [6, 5, 8, 9, 7]
+#     lst = lst.pop(4)
+#     return lst
+# print(rate())
+# def rate():
+#     lst = [20, 15, 26, 8, 4]
+#     lst = lst.pop(4)
+#     return lst
+# print(rate())
+
+#3-masala----------------
+# def create():
+#     lst = [
+#         [1, 2, 3, 4, 5]
+#         [5, 6, 7, 8, 9]
+#         [20, 21, 34, 56, 100]
+#     ]
+#     lst = lst.index(0)
+#
+#     return sum(lst)
+# print(create())
+
+
